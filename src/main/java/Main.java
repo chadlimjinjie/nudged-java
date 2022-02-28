720ce989fd0430d8570ffbe5a61e9f093945e3ff
Chad <chad_lim@ncs.com.sg>
2022-02-28 17:55:17 +0800

    Added more test and renamed variables

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,23 +6,35 @@ public class Main {
         ArrayList<Point> robot_map_pts = new ArrayList<Point>();
         ArrayList<Point> rmf_map_pts = new ArrayList<Point>();
 
-        robot_map_pts.add(new Point(1545, 911));
-        robot_map_pts.add(new Point(1995, 769));
-        robot_map_pts.add(new Point(852, 1985));
-        robot_map_pts.add(new Point(2686, 2941));
+//        robot_map_pts.add(new Point(1545, 911));
+//        robot_map_pts.add(new Point(1995, 769));
+//        robot_map_pts.add(new Point(852, 1985));
+//        robot_map_pts.add(new Point(2686, 2941));
+//
+//        rmf_map_pts.add(new Point(507.7, -825.4));
+//        rmf_map_pts.add(new Point(859.9, -955.3));
+//        rmf_map_pts.add(new Point(-19.32, 15.59));
+//        rmf_map_pts.add(new Point(1466, 756.5));
+        
+        robot_map_pts.add(new Point(347,608));
+        robot_map_pts.add(new Point(579,735));
+        robot_map_pts.add(new Point(782,339));
+        robot_map_pts.add(new Point(529,186));
 
-        rmf_map_pts.add(new Point(507.7, -825.4));
-        rmf_map_pts.add(new Point(859.9, -955.3));
-        rmf_map_pts.add(new Point(-19.32, 15.59));
-        rmf_map_pts.add(new Point(1466, 756.5));
+        rmf_map_pts.add(new Point(902,805));
+        rmf_map_pts.add(new Point(1154,939));
+        rmf_map_pts.add(new Point(1369,476));
+        rmf_map_pts.add(new Point(997,270));
 
-        Transformer rmf2robot_trans = new Transformer(robot_map_pts, rmf_map_pts);
+        Transformer rmf2robot_trans = new Transformer(rmf_map_pts, robot_map_pts);
+        Transformer robot2rmf_trans = new Transformer(robot_map_pts, rmf_map_pts);
 
-        Point robotMapCoord = rmf2robot_trans.transformPosition(2022, 2745);
+        Point rmfMapCoord = robot2rmf_trans.transformPosition(586, 677);
 
-        System.out.println(robotMapCoord.getX() + " " + robotMapCoord.getY());
-        System.out.println(convertMapToPosePositionX(robotMapCoord.getX(), -0.049, 0.050005815) + " "
-                + convertMapToPosePositionY(robotMapCoord.getY(), 0.016, 0.049994766));
+        System.out.println(rmfMapCoord.getX() + " " + rmfMapCoord.getY());
+//        System.out.println(convertMapToPosePositionX(rmfMapCoord.getX(), 0.049, 0.050005815) + " "
+//                + convertMapToPosePositionY(rmfMapCoord.getY(), 0.016, 0.049994766));
+
     }
 
     /**
